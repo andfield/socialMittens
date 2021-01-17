80055a5e25b54723a685fa6b4b6a80d06669e12b
siddharththakur76@gmail.com <siddharththakur76@gmail.com>
2021-01-17 13:52:18 +1300

    added find user posts route

--- server/keys.js
+++ server/keys.js
@@ -1,6 +1,7 @@
 module.exports={
     //Mongo connection url
-    mongoUri: "mongodb://sid:N3cacJsgRjmOGwt4@cluster0-shard-00-00.k9vk4.mongodb.net:27017,cluster0-shard-00-01.k9vk4.mongodb.net:27017,cluster0-shard-00-02.k9vk4.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-oujdia-shard-0&authSource=admin&retryWrites=true&w=majority",
+    mongoUri: 'mongodb://sid:1234@cluster0-shard-00-00.k9vk4.mongodb.net:27017,cluster0-shard-00-01.k9vk4.mongodb.net:27017,cluster0-shard-00-02.k9vk4.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-oujdia-shard-0&authSource=admin&retryWrites=true&w=majority',
+    //mongoUri: "mongodb://sid:N3cacJsgRjmOGwt4@cluster0-shard-00-00.k9vk4.mongodb.net:27017,cluster0-shard-00-01.k9vk4.mongodb.net:27017,cluster0-shard-00-02.k9vk4.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-oujdia-shard-0&authSource=admin&retryWrites=true&w=majority",
 
     //JWT secret Key
     JWT_SECRET: 'pleasehireme'
--- server/routes/post.js
+++ server/routes/post.js
@@ -7,6 +7,7 @@ const requireLogin = require('../middleware/requireLogin')
 
 //All the routes for post
 
+//Create new post route
 router.post('/createpost', requireLogin, (req,res) => {
     const {title, body} = req.body
     if(!title || !body) {
@@ -15,6 +16,7 @@ router.post('/createpost', requireLogin, (req,res) => {
     //Make password invisible on post
     req.user.password = undefined
 
+    //Create a new post
     const post = new Post({
         title,
         body,
@@ -31,7 +33,34 @@ router.post('/createpost', requireLogin, (req,res) => {
     })
 })
 
+//Get all posts
+router.get('/allpost', (req,res) => {
+    //find all the posts
+    Post.find()
+    //Populate the postedBy with user name and Id.
+    .populate("postedBy", '_id name')
+    .then(posts => {
+        return res.json({posts})
+    })
+    .catch(error => {
+        console.log(error)
+    })
+})
 
+//Get post created by a single user
+router.get('/mypost', requireLogin, (req,res) => {
+    //Get all the post posted by the current loggedin user
+    Post.find({postedBy: req.user._id})
+    //populate the posted by field
+    .populate("postedBy", "_id name")
+    .then(posts => {
+        res.json({posts})
+    })
+    .catch(error => {
+        console.log(error)
+    })
+
+})
 
 
 module.exports = router
