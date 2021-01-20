02213c214c55545fa0070c653de6e891a2a84be6
siddharththakur76@gmail.com <siddharththakur76@gmail.com>
2021-01-20 14:55:44 +1300

    restrictions on accessing routes depending on user state

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,20 +1,35 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {Link} from 'react-router-dom'
+import {UserContext} from '../App'
+const NavBar=() => {
 
-const NavBar = () => {
-    return(
-        <nav>
-        <div className="nav-wrapper #F4F39A yellow lighten-3">
-          <Link to="/" className="brand-logo left">Social Mittens</Link>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/signup">Sign up</Link></li>
-            <li><Link to="/profile">Profile</Link></li>
-            <li><Link to="/create">Create Post</Link></li>
-          </ul>
-        </div>
-      </nav>
-    )
+  const {state, dispatch}=useContext(UserContext)
+
+  const renderList=() => {
+    if (state) {
+      return [
+        <li><Link to="/profile">Profile</Link></li>,
+        <li><Link to="/create">Create Post</Link></li>
+      ]
+    }
+    else {
+      return [
+        <li><Link to="/login">Login</Link></li>,
+        <li><Link to="/signup">Sign up</Link></li>
+      ]
+    }
+  }
+
+  return (
+    <nav>
+      <div className="nav-wrapper #F4F39A yellow lighten-3">
+        <Link to={state? "/":"/login"} className="brand-logo left">Social Mittens</Link>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          {renderList()}
+        </ul>
+      </div>
+    </nav>
+  )
 }
 
 export default NavBar
