cbde2b8f2bafe9766d028962591242bdd066a571
siddharththakur76@gmail.com <siddharththakur76@gmail.com>
2021-01-21 17:19:23 +1300

    created like and unlike basic functions

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,7 +4,7 @@ import React, {useState, useEffect} from 'react'
 const Home=() => {
 
     //Post state
-    const [posts, setPosts]= useState([])
+    const [posts, setPosts]=useState([])
 
     //On mount get posts from server
     useEffect(() => {
@@ -16,7 +16,7 @@ const Home=() => {
             },
         })
             .then(res => res.json())
-            .then( result => {
+            .then(result => {
                 setPosts(result.posts)
             })
             .catch(error => {
@@ -24,12 +24,51 @@ const Home=() => {
             })
     }, [])
 
-    return (
+    //Create a function to like a post.
+    const likePost=(id) => {
 
+        //Create a fetch request to like the post
+        fetch('/like', {
+            method: 'put',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer "+localStorage.getItem('jwt')
+            },
+            body: JSON.stringify({
+                id: id
+            })
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+    }
+
+    //Create a function to unlike a post.
+    const unlikePost=(id) => {
+
+        //Create a fetch request to like the post
+        fetch('/unlike', {
+            method: 'put',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer "+localStorage.getItem('jwt')
+            },
+            body: JSON.stringify({
+                id:id
+            })
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+    }
+
+    return (
         // Main div
         <div className="home">
             {
-                posts.map( post => {
+                posts.map(post => {
                     return (
                         <div className="card home-card" key={post._id}>
                             <h5>{post.postedBy.name}</h5>
@@ -39,7 +78,9 @@ const Home=() => {
                             </div>
                             {/* Card contents */}
                             <div className="card-content">
-                                <i className="material-icons" style={{color: 'red'}} >favorite_border</i>
+                                <i className="material-icons" style={{color: 'blue'}} onClick={() => {likePost(post._id)}} >thumb_up</i>
+                                <i className="material-icons" style={{marginLeft: '10px', color: 'red'}} onClick={() => {unlikePost(post._id)}} >thumb_down</i>
+                                <h6>{post.likes.length} Likes</h6>
                                 <h6>{post.title}</h6>
                                 <p>{post.body}</p>
                                 <input type="text" placeholder="Add a comment" />
--- server/routes/post.js
+++ server/routes/post.js
@@ -2,25 +2,25 @@ const express=require('express')
 const router=express.Router()
 const mongoose=require('mongoose')
 const Post=mongoose.model("Post")
-const requireLogin = require('../middleware/requireLogin')
+const requireLogin=require('../middleware/requireLogin')
 
 
 //All the routes for post
 
 //Create new post route
-router.post('/createpost', requireLogin, (req,res) => {
-    const {title, body,imgURL} = req.body
-    if(!title || !body || !imgURL) {
+router.post('/createpost', requireLogin, (req, res) => {
+    const {title, body, imgURL}=req.body
+    if (!title||!body||!imgURL) {
         return res.status(422).json({error: "Please provide all the fields"})
     }
     //Make password invisible on post
-    req.user.password = undefined
+    req.user.password=undefined
 
     //Create a new post
-    const post = new Post({
+    const post=new Post({
         title,
         body,
-        image:imgURL,
+        image: imgURL,
         postedBy: req.user
 
     })
@@ -29,39 +29,80 @@ router.post('/createpost', requireLogin, (req,res) => {
     post.save().then(newPost => {
         res.json({post: newPost})
     })
-    .catch(error => {
-        console.log(error)
-    })
+        .catch(error => {
+            console.log(error)
+        })
 })
 
 //Get all posts
-router.get('/allpost', requireLogin, (req,res) => {
+router.get('/allpost', requireLogin, (req, res) => {
     //find all the posts
     Post.find()
-    //Populate the postedBy with user name and Id.
-    .populate("postedBy", '_id name')
-    .then(posts => {
-        return res.json({posts})
-    })
-    .catch(error => {
-        console.log(error)
-    })
+        //Populate the postedBy with user name and Id.
+        .populate("postedBy", '_id name')
+        .then(posts => {
+            return res.json({posts})
+        })
+        .catch(error => {
+            console.log(error)
+        })
 })
 
 //Get post created by a single user
-router.get('/mypost', requireLogin, (req,res) => {
+router.get('/mypost', requireLogin, (req, res) => {
     //Get all the post posted by the current loggedin user
     Post.find({postedBy: req.user._id})
-    //populate the posted by field
-    .populate("postedBy", "_id name")
-    .then(posts => {
-        res.json({posts})
-    })
-    .catch(error => {
-        console.log(error)
-    })
+        //populate the posted by field
+        .populate("postedBy", "_id name")
+        .then(posts => {
+            res.json({posts})
+        })
+        .catch(error => {
+            console.log(error)
+        })
 
 })
 
+//Like a post route
+router.put('/like', requireLogin, (req, res) => {
 
-module.exports = router
+    Post.findByIdAndUpdate(req.body.id, {
+        //push into the likes array
+        $push: {likes: req.user._id}
+    }, {
+        //To get new updated record from mongo
+        new: true
+    })
+    //execute the query
+    .exec((error, result) => {
+        if(error){
+            return res.status(422).json({error})
+        }
+        else{
+            res.json(result)
+        }
+    })
+})
+
+//unlike a post route
+router.put('/unlike', requireLogin, (req, res) => {
+    
+    Post.findByIdAndUpdate(req.body.id, {
+        //pull the user from likes array
+        $pull: {likes: req.user._id}
+    }, {
+        //To get new updated record from mongo
+        new: true
+    })
+    //execute the query
+    .exec((error, result) => {
+        if(error){
+            return res.status(422).json({error})
+        }
+        else{
+            res.json(result)
+        }
+    })
+})
+
+module.exports=router
