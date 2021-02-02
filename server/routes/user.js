3d794434ad940e2114737ebfb651aed57c97604c
siddharththakur76@gmail.com <siddharththakur76@gmail.com>
2021-02-02 16:07:43 +1300

    follow unfollow api made

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,30 +2,92 @@ const express=require('express')
 const router=express.Router()
 const mongoose=require('mongoose')
 const Post=mongoose.model("Post")
-const User = mongoose.model("User")
+const User=mongoose.model("User")
 const requireLogin=require('../middleware/requireLogin')
 
 //see profile of a user.
-router.get('/user/:id',requireLogin, (req, res) => {
+router.get('/user/:id', requireLogin, (req, res) => {
 
     User.findOne({_id: req.params.id})
-    .select("-password")
-    .then(user => {
-        Post.find({postedBy: req.params.id})
-        .populate("postedBy", "_id name")
-        .exec((error, posts) => {
-            if(error){
-                res.status(422).json({error})
-            }
-            else{
-                res.json({user, posts})
-            }
+        .select("-password")
+        .then(user => {
+            Post.find({postedBy: req.params.id})
+                .populate("postedBy", "_id name")
+                .exec((error, posts) => {
+                    if (error) {
+                        res.status(422).json({error})
+                    }
+                    else {
+                        res.json({user, posts})
+                    }
+                })
         })
-    })
-    .catch(error => {
-        return res.status(404).json({error: "User not Found"})
-    })
+        .catch(error => {
+            return res.status(404).json({error: "User not Found"})
+        })
 
+})
+
+//Follow users.
+router.put('/follow', requireLogin, (req, res) => {
+    User.findByIdAndUpdate(req.body.followId, {
+        $push: {followers: req.user._id}
+    },
+        {
+            new: true
+        },
+        (error, result) => {
+            if (error) {
+                return res.status(422).json({error})
+            }
+            else {
+                User.findByIdAndUpdate(req.user._id, {
+                    $push: {following: req.body.followId}
+                },
+                    {
+                        new: true
+                    }
+                )
+                    .then(result => {
+                        res.json(result)
+                    })
+                    .catch(error => {
+                        res.status(422).json({error})
+                    })
+            }
+        }
+    )
+})
+
+//UnFollow users.
+router.put('/follow', requireLogin, (req, res) => {
+    User.findByIdAndUpdate(req.body.followId, {
+        $pull: {followers: req.user._id}
+    },
+        {
+            new: true
+        },
+        (error, result) => {
+            if (error) {
+                return res.status(422).json({error})
+            }
+            else {
+                User.findByIdAndUpdate(req.user._id, {
+                    $pull: {following: req.body.followId}
+                },
+                    {
+                        new: true
+                    }
+                )
+                    .then(result => {
+                        res.json(result)
+                    })
+                    .catch(error => {
+                        res.status(422).json({error})
+                    })
+            }
+        }
+    )
 })
 
 
