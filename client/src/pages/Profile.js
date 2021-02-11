66da0fa60bc6a6c00836b069777d744de3ae7d05
siddharththakur76@gmail.com <siddharththakur76@gmail.com>
2021-02-11 22:44:52 +1300

    design changes on profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,7 +4,7 @@ import {UserContext} from '../App'
 const Profile=() => {
 
     //use context to get access to user state
-    const {state, dispatch} = useContext(UserContext)
+    const {state, dispatch}=useContext(UserContext)
 
     //state to save all user posts
     const [myposts, setMyposts]=useState([])
@@ -28,29 +28,40 @@ const Profile=() => {
     return (
         // Page Div
         <div style={{maxWidth: '550px', margin: '0px auto'}}>
+
             {/* Image and details */}
             <div style={{
                 display: 'flex',
                 justifyContent: 'space-around',
                 margin: '18px 0px',
-                borderBottom: '1px solid grey'
+                borderBottom: '2px solid grey'
             }}>
                 {/* Image div */}
-                <div>
+                <div className="img-hover" >
                     <img style={{width: "160px", height: "160px", borderRadius: "80px"}}
-                        src={state ? state.profilePicture : "Loading..."}
+                        src={state? state.profilePicture:"Loading..."}
+                        className="image"
                     />
+                    {/* Image Update button */}
+                    <div className="hover-btn" style={{marginLeft: '20px'}}>
+                        <a
+                            onClick={null} >
+                            <i className="material-icons">add_a_photo</i>
+                        </a>
+                    </div>
+
                 </div>
+
                 {/* User details */}
                 <div>
                     <h4>
-                       {state?state.name:"loading user"}
+                        {state? state.name:"loading user"}
                     </h4>
-                    <h5>{state?state.email: "loading email"}</h5>
+                    <h5>{state? state.email:"loading email"}</h5>
                     <div style={{display: 'flex', justifyContent: 'space-between', width: '108%'}}>
                         <h6>{myposts.length} Post</h6>
-                        <h6>{state ? state.followers.length : "0"} followers</h6>
-                        <h6>{ state ? state.following.length : "0"} following</h6>
+                        <h6>{state? state.followers.length:"0"} followers</h6>
+                        <h6>{state? state.following.length:"0"} following</h6>
                     </div>
                 </div>
             </div>
